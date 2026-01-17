ae2a33a291fa50ae64d5712a15ab5429f3cc6d02
Perez <perezfopa20@gmail.com>
2026-01-17 23:13:27 +0100

    Modifications des attibuts de chaque classe
    classe course : id, name, semester, year, description
    classe document : id, title, doc_type, content
    classe event : id, title, event_type, date, description
    classe grade : id, course_id, value (valeur de la note), max_value

--- models/course.py
+++ models/course.py
@@ -1,8 +1,10 @@
 class course:
-    def __init__(self, id, name, description=None):
+    def __init__(self, id, name, semester, year, description=None):
         self.id = id
         self.name = name
+        self.semester = semester
+        self.year = year
         self.description = description
 
     def __str__(self):
-        return f"Course(id={self.id}, name='{self.name}', description='{self.description}')"
+        return f"Course(id={self.id}, name='{self.name}', semester='{self.semester}', year={self.year}, description='{self.description}')"
